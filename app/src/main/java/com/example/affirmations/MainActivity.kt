907db4be2631962402f77be07ed7cc006bbcdae0
rodriguesporan <rodriguesporan@gmail.com>
2021-02-20 17:55:27 -0300

    feat: initialized project

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+        RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    /**
+     * Defining a class inside another class is called creating a nested class.
+     *
+     * Since ItemViewHolder is only used by ItemAdapter,
+     * creating it inside ItemAdapter shows this relationship.
+     * This is not mandatory,
+     * but it helps other developers understand the structure of your program
+     */
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context) // get an instance of LayoutInflater
+                .inflate(R.layout.list_item, parent, false) // inflate the list item view
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount() = dataset.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getText(item.stringResourceId)
+    }
+}
