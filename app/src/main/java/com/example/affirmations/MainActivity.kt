cf58af7b68306f701fd0b91e2027959c3bcfdccb
rodriguesporan <rodriguesporan@gmail.com>
2021-02-20 22:49:15 -0300

    feat: using binding to inflate layout to recicleview

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 import com.example.affirmations.databinding.ActivityMainBinding
@@ -18,11 +17,10 @@ class MainActivity : AppCompatActivity() {
         // Initialize data
         val myDataset = Datasource().loadAffirmations()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
+        binding.recyclerView.adapter = ItemAdapter(this, myDataset)
 
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true)
+        binding.recyclerView.setHasFixedSize(true)
     }
 }
